3a06b0dce167f39fab39cd52c46de58ecc4aae23
airfutur <airfutur@msn.com>
2016-10-21 11:56:22 +0200

    Partes 3-5
    Partes 3-5 de la practica

--- Practica-5/Item.js
+++ Practica-5/Item.js
@@ -2,15 +2,33 @@
  * Created by boyander on 11/10/16.
  */
 
-var Item = function Item(nombre, imagen, precio){
-    this.title = nombre;
-    this.imagen = imagen;
-    this.price = precio;
+var Item = function Item(imagen){
+    this.image = imagen;
+    this.talla = "S";
+    this.unidades = 1;
+    this.price = 0.00;
 };
 
 // Get random price in range min, max
 Item.prototype.getPrice = function(){
-   return this.price;
+    var min = 15;
+    var max = 200;
+    return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+Item.prototype.getTalla = function(){
+    var min = 1;
+    var max = 3;
+    var res = Math.floor((Math.random() * max) + min);
+    if (res == 1){return "S"}
+    else if (res == 2){return "M"}
+    else return "L"
+}
+
+Item.prototype.getUnidades = function(){
+    var min = 0;
+    var max = 1000;
+    return Math.floor((Math.random() * max) + min);
 }
 
 exports.Item = Item;
--- Practica-5/app.js
+++ Practica-5/app.js
@@ -22,7 +22,7 @@ app.use("/public",express.static('public'));
 var API500px = require('500px');
 var api500px = new API500px("YecP85RjzG08DN0MqvgFa0N780dNaDmJX6iTPbYp");
 var pics = [];
-api500px.photos.getPopular({'sort': 'created_at', 'rpp': '10','image_size':200},  function(error, results) {
+api500px.photos.searchByTerm("clothes",{'sort': 'created_at', 'rpp': '10','image_size':200},  function(error, results) {
     // Do something
     pics = results.photos.map(function(a){
         // Compose object to be used in show items template
@@ -33,14 +33,8 @@ api500px.photos.getPopular({'sort': 'created_at', 'rpp': '10','image_size':200},
 // Render frontpage
 app.get('/', function (req, res) {
     res.render('portada',{
-        pics: [
-            new Item("Zapatillas", "http://nikeoutletweb.com/wp-content/uploads/2014/10/zapatillas-nike-outlet.jpg", "50"),
-            new Item("Chaqueta", "http://www.motonity.com/server/Portal_0003572/img/products/chaqueta-revit-roamer_755139_12570486.jpg", "150"),
-            new Item("Leging","http://www.engineered-life.com/wp-content/uploads/2015/11/ceto-legging-back.jpg","20"),
-            new Item("Zapatillas2", "http://nikeoutletweb.com/wp-content/uploads/2014/10/zapatillas-nike-outlet.jpg", "50"),
-            new Item("Chaqueta2", "http://www.motonity.com/server/Portal_0003572/img/products/chaqueta-revit-roamer_755139_12570486.jpg", "150"),
-            new Item("Leging2","http://www.engineered-life.com/wp-content/uploads/2015/11/ceto-legging-back.jpg","20")
-        ]
+        pics: pics
+
     });
 });
 
