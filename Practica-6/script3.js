b6fd624441a15f4496baf7a178ccd3d6d235dacf
airfutur <airfutur@msn.com>
2016-10-28 08:42:24 +0200

    Delete script3.js

--- Practica-6/script3.js
+++
@@ -1,6 +0,0 @@
-var myVar = setInterval(myTimer, 1000);
-function myTimer() {
-    var d = new Date();
-// Imprimimos la fecha actual en el elemento <p id="demo"></p> document.getElementById("demo").innerHTML = d.toLocaleTimeString();
-}
-
