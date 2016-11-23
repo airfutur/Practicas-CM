32359ba7c10be70a56f30b42d01bcfcedd3e83ab
airfutur <airfutur@msn.com>
2016-11-23 13:46:30 +0100

    Primer intento de la practica.No carga libreria Phaser estando bien detallada ¿?¿?¿?

--- P9/app.js
+++ P9/app.js
@@ -1,24 +1,39 @@
 /**
  * Created by parallels on 11/22/16.
  */
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create});
 
 function preload() {
 
-    game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
+    // game.load.spritesheet('button', 'assets/button_sprite_sheet.png', 193, 71);
     // 193x71 es el tamaño de cada cuadro, hay 3 en la imagen
-    game.load.image('background','assets/starfield.jpg');
+    game.load.image('boton-0','icons/siete.png');
+    game.load.image('boton-1','icons/cuatro.png');
+    game.load.image('boton-2','icons/uno.png');
+    game.load.image('boton-3','icons/cero.png');
+    game.load.image('boton-4','icons/ocho.png');
+    game.load.image('boton-5','icons/cinco.png');
+    game.load.image('boton-6','icons/dos.png');
+    game.load.image('boton-7','icons/igual.png');
+    game.load.image('boton-8','icons/nueve.png');
+    game.load.image('boton-9','icons/seis.png');
+    game.load.image('boton-10','icons/tres.png');
+    game.load.image('boton-11','icons/division.png');
+    game.load.image('boton-12','icons/clear.png');
+    game.load.image('boton-13','icons/mas.png');
+    game.load.image('boton-14','icons/menos.png');
+    game.load.image('boton-15','icons/por.png');
 
 }
 
 var button;
-var background;
+var a="",b="",c=0,res=0,llave="";
 
 function create() {
 
-    game.stage.backgroundColor = '#182d3b';
+    game.stage.backgroundColor = '#DDDDDD';
+    var style = { font: "32px Arial", fill: "#ff0044", align: "center" };
 
-    background = game.add.tileSprite(0, 0, 800, 600, 'background');
     var n=0;
     for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
@@ -27,21 +42,88 @@ function create() {
             n++;
         }
     }
+
 }
 // Cuadro 2 para raton por encima, cuadro 1 para fuera y cuadro 0 para cuando se clic
 
-//var a=0,b=0;
 function actionOnClick () {
-    console.log("Tecla pulsada: "+this.keyname);
-    /* c=0;
-    if (llave=="button-1"){
-        if (c=0)a=7;
-        else if (c=1) b=7;
-        else console.log('Error');
+    llave=this.keyname;
+
+    // console.log("Tecla pulsada: "+this.keyname);
+
+    if(llave==7){
+
+        if (c==13){
+            res=Number(a)+Number(b);
+        }
+        else if (c==14){
+            res=Number(a)-Number(b);
+        }
+        else if (c==15){
+            res=Number(a)*Number(b);
+        }
+        else if (c==11){
+            res=Number(a)/Number(b);
+        }
+        else res=Number(a);
+
+       game.add.text(40, 440, "RESULTADO: "+String(res), style);
+
+        //Falta renderizar el resultado por pantalla en este punto
+
+        res=0;
+        c=0;
     }
-    else if (llave=="button-2"){
-        accion_boton_2();
+    else if (llave==12){
+        res=0;
+
+        game.add.text(40, 440, "RESULTADO: "+String(res), style);
+    }}
+
+    else if (llave == (11||13||14||15){
+        c=llave;
     }
-    */
+
+    else if (llave==0){
+        if(c==0) a+=String(7);
+        else b+=String(7);
+    }
+    else if (llave==1){
+        if(c==0) a+=String(4);
+        else b+=String(4);
+    }
+    else if (llave==2){
+        if(c==0) a+=String(1);
+        else b+=String(1);
+    }
+    else if (llave==3){
+        if(c==0) a+=String(0);
+        else b+=String(0);
+    }
+    else if (llave==4){
+        if(c==0) a+=String(8);
+        else b+=String(8);
+    }
+    else if (llave==5){
+        if(c==0) a+=String(5);
+        else b+=String(5);
+    }
+    else if (llave==6){
+        if(c==0) a+=String(2);
+        else b+=String(2);
+    }
+    else if (llave==8){
+        if(c==0) a+=String(9);
+        else b+=String(9);
+    }
+    else if (llave==9){
+        if(c==0) a+=String(6);
+        else b+=String(6);
+    }
+    else if (llave==10){
+        if(c==0) a+=String(3);
+        else b+=String(3);
+    }
 }
+
 
